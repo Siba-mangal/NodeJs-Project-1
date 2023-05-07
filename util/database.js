f275f5a4ba75d0d2c25e5dc393df4ddd76e92758
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-07 12:12:29 +0530

    Add files via upload

--- util/database.js
+++ util/database.js
@@ -1,1 +1,7 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("food-order", "root", "Siba@2518", {
+  dialect: "mysql",
+  host: "localhost",
+});
 
+module.exports = sequelize;
