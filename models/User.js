3985e3c75d7958817d38f50ad13a03ff716ea8a3
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-07 12:13:05 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,1 +1,26 @@
+const Sequelize = require("sequelize");
 
+const sequelize = require("../util/database");
+
+const User = sequelize.define("users", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  productName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  tableNo: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
